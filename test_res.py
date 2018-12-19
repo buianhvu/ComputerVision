01ba69ab7da31ed50572798b11f9a96c83f2d157
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-19 17:10:37 +0700

    save change

--- test_res.py
+++ test_res.py
@@ -7,7 +7,7 @@ from commons.cv_input import *
 if __name__ == '__main__':
     net = res_net101(num_classes=11)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net = load_data(net,model_name="res_101_food", path='content/drive/My Drive/output' ,device=device)
+    net = load_data(net, model_name="res_101_food", path='/content/drive/My Drive/output/' ,device=device)
 
     _, test_loader = get_loader(root='/content/drive/My Drive/Food-11/',inside="evaluation")
     classes = tuple(range(11))
