b1c3367b452620928fb2f60c7dec6349ce77dcfe
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-20 10:06:32 +0700

    save change

--- run_res2.py
+++ run_res2.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     net = train_model(net, train_loader, model_name="res_101_food", optimize_func=OPT["adam"],
-                momentum=None, path_state="/content/drive/My Drive/output/")
+                momentum=None, path_state="/content/drive/My Drive/output/", epoch_num=10)
 
     _, test_loader = get_loader(root='/content/drive/My Drive/Food-11',inside="evaluation")
     classes = tuple(range(11))
