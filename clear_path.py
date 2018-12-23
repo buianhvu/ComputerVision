2ceed4e1f0298b883b0cad3482e017a932b79f81
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-23 11:13:02 +0700

    adding change

--- clear_path.py
+++
@@ -1,5 +0,0 @@
-from train.train import *
-
-if __name__ == '__main__':
-    # clear_path("res_food", "/content/drive/My Drive/output", "/content/drive/My Drive/output")
-    clear_path("se_food", "/content/drive/My Drive/output", "/content/drive/My Drive/output")
---
+++ clear_se.py
@@ -0,0 +1,7 @@
+from train.train import *
+
+
+if __name__ == '__main__':
+    model_name = "se_food"
+    output_folder = "/content/drive/My Drive/output/" + model_name
+    clear_path(model_name, output_folder, output_folder)
