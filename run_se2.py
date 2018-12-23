e9b795a8efa61ad25a4becd6c0ccb5362bff3502
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-23 15:12:10 +0700

    number of workers = batch_size

--- run_se2.py
+++ run_se2.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
     num_classes = 11
     input_folder = "/content/drive/My Drive/Food-11"
     output_folder = "/content/drive/My Drive/output/" + model_name
-    batch_size = 35
+    batch_size = 40
     epoch_num = 50
 
     net = res_se_50(num_classes=num_classes)
 
-    _, train_loader = get_loader(root=input_folder, batch_size=batch_size)
+    _, train_loader = get_loader(root=input_folder, batch_size=batch_size, number_of_workers=batch_size)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     net = train_model_ac_load_save(net, train_loader, model_name=model_name, optimize_func=OPT["adam"],
                                    momentum=None, path_state=output_folder,
                                    path_log=output_folder, epoch_num=epoch_num)
 
-    _, test_loader = get_loader(root=input_folder, inside="evaluation", batch_size=batch_size)
+    _, test_loader = get_loader(root=input_folder, inside="evaluation", batch_size=batch_size, number_of_workers=batch_size)
     classes = tuple(range(num_classes))
     test_data(model=net, test_loader=test_loader, classes=classes, device=device, file_log=output_folder)
 
