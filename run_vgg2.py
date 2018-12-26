8eb3a34204d31d7e94423476b904076036a514d7
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-26 09:12:59 +0700

    fix vgg

--- run_vgg2.py
+++ run_vgg2.py
@@ -5,14 +5,14 @@ from commons.cv_input import *
 
 
 if __name__ == '__main__':
-    model_name = "res_food"
+    model_name = "vgg_food"
     num_classes = 11
     input_folder = "/content/drive/My Drive/Food-11"
     output_folder = "/content/drive/My Drive/output/" + model_name
     batch_size = 50
     epoch_num = 50
 
-    net = set_vgg16(num_classes=11, defaul_last_size=(7, 7))
+    net = set_vgg16(num_classes=11, default_last_size=(7, 7))
 
     _, train_loader = get_loader(root=input_folder, batch_size=batch_size)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
