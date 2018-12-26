efa86665b827582d68aedaf88fd7ad7b14485e73
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-26 09:20:42 +0700

    adding to clear_vgg

--- clear_vgg.py
+++ clear_vgg.py
@@ -1,7 +1,9 @@
 from train.train import *
+import os
 
 
 if __name__ == '__main__':
     model_name = "vgg_food"
     output_folder = "/content/drive/My Drive/output/" + model_name
+    os.makedirs(output_folder, exist_ok=True)
     clear_path(model_name, output_folder, output_folder)
