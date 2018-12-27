f9daf0dbc065aef3c2ee4db0c355b271c0b36838
Dinh Dung <dinhanhdung1996@gmail.com>
2018-12-27 15:29:02 +0700

    se 34

--- model/se_net.py
+++ model/se_net.py
@@ -174,6 +174,7 @@ def res_se_50(num_classes=10):
 
 def res_se_34(num_classes=10):
     model = ResNet(ResBasicSeBlock, [3, 2, 3, 3], num_classes=num_classes)
+    return model
 
 
 # from model.res_net import *
